4b0db0b0e7789ce7ec257b9ba1e9d2bf7574fcc1
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-01-19 15:02:02 -0300

    Componente: TransactionsTable

--- front-end/dtmoney/src/components/Dashboard/index.tsx
+++ front-end/dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary';
+import { TransActionsTable } from '../TransactionsTable';
 import { Container } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransActionsTable />
     </Container>
   );
 }
