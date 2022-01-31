2cdf6ca9cb58c8f74aadb501c19c06daa714da91
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-01-31 12:09:21 -0300

    Página Home

--- front-end/ignews/src/pages/index.tsx
+++ front-end/ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,7 +7,20 @@ export default function Home() {
       <Head>
         <title> Home | IG.news</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span>
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girld codding" />
+      </main>
     </>
   );
 }
