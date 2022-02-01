2291bea5f1febeac29bfb13d3d3b5ac4d775907f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-02-01 07:47:06 -0300

    Componente: SubscribeButton

--- front-end/ignews/src/pages/index.tsx
+++ front-end/ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -17,9 +18,10 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
-        <img src="/images/avatar.svg" alt="girld codding" />
+        <img src="/images/avatar.svg" alt="girl codding" />
       </main>
     </>
   );
