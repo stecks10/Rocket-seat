d53dfed8b7f2b34f41532bb56e8311b6bbd445f0
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-04-26 13:53:14 -0300

    get

---
+++ APIs/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.listen('3000')
+
+app.route('/').get( (req, res) => res.send("Hello"))
+app.route('/sobre').get( (req, res) => res.send("Hello sobre"))
