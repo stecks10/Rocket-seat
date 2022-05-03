21122284b7d0baa7a7b0fc24e331239f521e13f0
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-05-03 08:58:56 -0300

    aula1-impulse

---
+++ front-end/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import styles from 'styles'
+
+export function Home() {
+  return (
+    <>
+      <div className="container">
+        <h1>Lista de Presença</h1>
+        <input type="text" placeholder="Digite o nome..." />
+        <button type="button">Adicionar</button>
+      </div>
+    
+    </>
+  )
+}
+
