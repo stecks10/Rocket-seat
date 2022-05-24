563cf6c8d9e719dc54d6c5b98bbebbea666466ab
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-05-24 16:36:24 -0300

    estado

--- front-end/reactapp/src/components/Card/index.jsx
+++ front-end/reactapp/src/components/Card/index.jsx
@@ -1,10 +1,10 @@
 import './styles.css';
 
-export function Card() {
+export function Card({ name, time }) {
   return (
     <div className="card">
-      <strong>Vitor Nunes</strong>
-      <small>10:15:23</small>
+      <strong>{name}</strong>
+      <small>{time}</small>
     </div>
   );
 }
--- front-end/reactapp/src/pages/Home/index.jsx
+++ front-end/reactapp/src/pages/Home/index.jsx
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
 import './styles.css';
 
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [studentName, setStudentName] = useState('');
+
   return (
     <>
       <div className="container">
-        <h1>Lista de Presença</h1>
-        <input type="text" placeholder="Digite o nome..." />
+        <h1>Nome : {studentName} </h1>
+        <input
+          type="text"
+          placeholder="Digite o nome..."
+          onChange={(e) => setStudentName(e.target.value)}
+        />
         <button type="button">Adicionar</button>
 
-        <Card />
+        <Card name="Vitor" time="15:55:25" />
+        <Card name="Joao" time="10:55:25" />
       </div>
     </>
   );
