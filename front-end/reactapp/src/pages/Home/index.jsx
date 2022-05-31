dea0b0e53ed312992cf5bc3ad9385b9a42101256
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-05-31 13:05:21 -0300

    async await

--- front-end/reactapp/src/pages/Home/index.jsx
+++ front-end/reactapp/src/pages/Home/index.jsx
@@ -23,14 +23,16 @@ export function Home() {
   }
 
   useEffect(() => {
-    fetch('https://api.github.com/users/stecks10')
-      .then((response) => response.json())
-      .then((data) => {
-        setUser({
-          name: data.name,
-          avatar: data.avatar_url,
-        });
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/stecks10');
+      const data = await response.json();
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
       });
+    }
+
+    fetchData();
   }, []);
 
   return (
