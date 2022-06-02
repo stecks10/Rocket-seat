8328e65899cff385166ee12e9df05bee494fa41a
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-06-02 10:46:06 -0300

    propriedades

--- ignite/01-fundamentos-reactjs/src/App.jsx
+++ ignite/01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>;
+import { Post } from './Post';
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Vitor Nunes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur ducimus animi aliquam at laboriosam quidem et quo consequuntur, illum maiores, distinctio tempora exercitationem quis ad ipsum temporibus voluptate nulla."
+      />
+      <Post author="User" content="Novopost" />
+    </div>
+  );
 }
-
-export default App;
