c4a2e379c1db27e78e5af4c15807ebcc57b8af22
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-06-03 10:17:29 -0300

    cssglobal

--- ignite/01-fundamentos-reactjs/src/App.jsx
+++ ignite/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,18 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 export function App() {
   return (
-    <div>
-      <Post
-        author="Vitor Nunes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur ducimus animi aliquam at laboriosam quidem et quo consequuntur, illum maiores, distinctio tempora exercitationem quis ad ipsum temporibus voluptate nulla."
-      />
-      <Post author="User" content="Novopost" />
-    </div>
+    <>
+      <Header />
+      <div>
+        <Post
+          author="Vitor Nunes"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur ducimus animi aliquam at laboriosam quidem et quo consequuntur, illum maiores, distinctio tempora exercitationem quis ad ipsum temporibus voluptate nulla."
+        />
+        <Post author="User" content="Novopost" />
+      </div>
+    </>
   );
 }
