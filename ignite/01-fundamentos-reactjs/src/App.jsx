273b76a17c0cd16c43dc108b6cc294282fedf221
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-06-04 15:23:57 -0300

    sidebar

--- ignite/01-fundamentos-reactjs/src/App.jsx
+++ ignite/01-fundamentos-reactjs/src/App.jsx
@@ -1,17 +1,19 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
+
+import styles from './App.module.css';
 
 import './global.css';
 export function App() {
   return (
     <>
       <Header />
-      <div>
-        <Post
-          author="Vitor Nunes"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur ducimus animi aliquam at laboriosam quidem et quo consequuntur, illum maiores, distinctio tempora exercitationem quis ad ipsum temporibus voluptate nulla."
-        />
-        <Post author="User" content="Novopost" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Vitor" content="loredasudasuh"></Post>
+        </main>
       </div>
     </>
   );
---
+++ ignite/01-fundamentos-reactjs/src/components/Sidebar/index.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1635830625698-3b9bd74671ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=300&q=60"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/80498486?v=4"
+        />
+
+        <strong>Vitor Nunes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
