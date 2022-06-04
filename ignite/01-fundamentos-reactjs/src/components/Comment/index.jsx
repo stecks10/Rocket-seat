7335be2a9d668ba434d11b2fc234f96f6fe0c11c
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-06-04 17:08:19 -0300

    component comment

---
+++ ignite/01-fundamentos-reactjs/src/components/Comment/index.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './styles.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/stecks10.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vitor Nunes</strong>
+              <time title="04 de Junho as 15:45" dateTime="2022-06-04 08:13:20">
+                Cerca de 1h atras
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabens 🌹🌹</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
