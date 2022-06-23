95f8689b5a4491346479bdd5f78ca0cb11e2346c
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-06-23 10:45:52 -0300

    Aula-02

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,26 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { Sidebar } from "./components/Sidebar";
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY =  gql`
-  query {
-    lessons  {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 export function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
   return (
-    <ul>
-      {data?.lessons.map(lessons => {
-        return <li key={lessons.id}>{lessons.title}</li>
-      })}
-    </ul>
+    <>
+      <Event />
+    </>
   )
 }
