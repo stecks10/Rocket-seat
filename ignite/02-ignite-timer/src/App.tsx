1a9fc797c2fed12bd0d360dd66c66e3871f1b946
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-07-11 08:09:02 -0300

    tema global

--- ignite/02-ignite-timer/src/App.tsx
+++ ignite/02-ignite-timer/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
@@ -9,6 +10,8 @@ export function App() {
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
