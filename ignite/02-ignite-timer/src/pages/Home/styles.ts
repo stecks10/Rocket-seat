b85fb592079e1db613bbaf77dc5de11f50513e2d
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-08-17 09:20:22 -0300

    interromper ciclo

--- ignite/02-ignite-timer/src/pages/Home/styles.ts
+++ ignite/02-ignite-timer/src/pages/Home/styles.ts
@@ -86,7 +86,7 @@ export const Separator = styled.div`
   justify-content: center;
 `;
 
-export const StarCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -96,19 +96,30 @@ export const StarCountdownButton = styled.button`
   align-items: center;
   justify-content: center;
 
+  color: ${(props) => props.theme['gray-100']};
+
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`;
+
+export const StarCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `;
