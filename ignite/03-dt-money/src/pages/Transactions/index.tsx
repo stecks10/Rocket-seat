98ba930d3e8605e7651d336588302c90a7a0f506
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-09-22 19:00:37 -0300

    tabela de transação

--- ignite/03-dt-money/src/pages/Transactions/index.tsx
+++ ignite/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site </td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000.00</PriceHighlight>
+              </td>
+
+              <td>venda</td>
+              <td>13/04/22</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Conta </td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 2.000.00</PriceHighlight>
+              </td>
+              <td>venda</td>
+              <td>13/04/22</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
