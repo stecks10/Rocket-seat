4b3a61cb4a2410350b4c60693d6697d70881c2e8
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-09-26 20:13:46 -0300

    formatando valores

--- ignite/03-dt-money/src/components/Summary/index.tsx
+++ ignite/03-dt-money/src/components/Summary/index.tsx
@@ -1,7 +1,8 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import { useContext } from 'react';
-import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
-import { SummaryContainer, SummaryCard } from './styles';
+import { priceFormatter } from '../../utils/formatter';
+import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
@@ -33,23 +34,25 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
+
       <SummaryCard>
         <header>
-          <span>Saidas</span>
+          <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
+
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
