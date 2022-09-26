e35dbe654329ab1be7c9441689ba0d1b9ad35240
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-09-26 10:39:38 -0300

    calc

--- ignite/03-dt-money/src/pages/Transactions/index.tsx
+++ ignite/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,35 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { SearchForm } from './components/SearchForm';
+
 import {
   PriceHighlight,
   TransactionsContainer,
   TransactionsTable,
 } from './styles';
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
-
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch(' http://localhost:3333/transactions');
-    const data = await response.json();
-
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>
@@ -38,6 +20,7 @@ export function Transactions() {
 
       <TransactionsContainer>
         <SearchForm />
+
         <TransactionsTable>
           <tbody>
             {transactions.map((transaction) => {
@@ -49,7 +32,6 @@ export function Transactions() {
                       {transaction.price}
                     </PriceHighlight>
                   </td>
-
                   <td>{transaction.category}</td>
                   <td>{transaction.createdAt}</td>
                 </tr>
