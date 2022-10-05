7346444b41d9b4a1c85b1a73a95957945aebb3de
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-10-05 06:15:55 -0300

    maxWidth

--- ignite/04-ignite-shop/src/styles/pages/home.ts
+++ ignite/04-ignite-shop/src/styles/pages/home.ts
@@ -3,8 +3,8 @@ import { styled } from '..';
 export const HomeContainer = styled('main', {
   display: 'flex',
   gap: '3rem',
-  width: '75%',
-  maxWidth: 'calc(100vw - ((100vw) - 1180) /2)',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
   minHeight: 656,
 });
@@ -47,6 +47,7 @@ export const Product = styled('a', {
     strong: {
       fontSize: '$lg',
     },
+
     span: {
       fontSize: '$xl',
       fontWeight: 'bold',
