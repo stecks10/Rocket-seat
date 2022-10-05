735fd3665c1398aeb728840d6e8a42afa7492461
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-10-05 06:15:01 -0300

    estrutura Home

--- ignite/04-ignite-shop/src/pages/_app.tsx
+++ ignite/04-ignite-shop/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { globalStyles } from '../styles/global';
 import logoImg from '../assets/logo.svg';
 import { Container, Header } from '../styles/pages/app';
 
-import Image from 'next/future/image'
+import Image from 'next/future/image';
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
--- ignite/04-ignite-shop/src/pages/index.tsx
+++ ignite/04-ignite-shop/src/pages/index.tsx
@@ -1,14 +1,31 @@
-import { styled } from '../styles';
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/future/image';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-  backgroundColor: '$gray300',
-});
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
 
 export default function Home() {
   return (
-    <Button>
-      <span>teste</span>
-      clique
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
