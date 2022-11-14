f7da084b336c429fe1791ccf3a35c5189555c28a
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-11-14 13:42:18 -0300

    Carregando dados do produto

--- ignite/04-ignite-shop/src/pages/product/[id].tsx
+++ ignite/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,28 +1,65 @@
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
+import { stripe } from '../../lib/stripe';
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,80</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-          commodi, sint explicabo ducimus magnam dolore, odit ipsa quam et autem
-          eum sunt saepe expedita sapiente molestiae similique, nostrum hic
-          quos!
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>Comprar Agora </button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productID = params.id;
+
+  const product = await stripe.products.retrieve(productID, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
