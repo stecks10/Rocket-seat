3d773b14470fa307c347e02b7f179f3d87f117e9
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-11-30 11:12:44 -0300

    Trabalhando com SEO

--- ignite/04-ignite-shop/src/pages/success.tsx
+++ ignite/04-ignite-shop/src/pages/success.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Stripe from 'stripe';
@@ -15,20 +16,28 @@ interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada!</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageContainer>
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <p>
-        Uhul <strong>{customerName}</strong>, sua{' '}
-        <strong>{product.name}</strong> foi comprada!
-      </p>
+      <SuccessContainer>
+        <h1>Compra efetuada!</h1>
 
-      <Link href="/">Voltar ao catálogo</Link>
-    </SuccessContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
+
+        <p>
+          Uhul <strong>{customerName}</strong>, sua{' '}
+          <strong>{product.name}</strong> foi comprada!
+        </p>
+
+        <Link href="/">Voltar ao catálogo</Link>
+      </SuccessContainer>
+    </>
   );
 }
 
