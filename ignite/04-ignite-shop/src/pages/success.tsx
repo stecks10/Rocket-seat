e0ba96f992070d352a3ac948c565b144ab884466
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-11-30 10:55:52 -0300

    Dados da compra no sucesso

--- ignite/04-ignite-shop/src/pages/success.tsx
+++ ignite/04-ignite-shop/src/pages/success.tsx
@@ -1,19 +1,54 @@
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
+import Stripe from 'stripe';
+import { stripe } from '../lib/stripe';
 import { ImageContainer, SuccessContainer } from '../styles/pages/success';
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhul <strong>Vitor Nunes</strong>, sua <strong>Camiseta</strong> foi
-        comprada!
+        Uhul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> foi comprada!
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
+
+  const customerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
