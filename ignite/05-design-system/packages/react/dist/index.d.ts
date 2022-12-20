d0a293eb28846ed7f368133cb6e80392a44c17f1
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-12-20 20:17:33 -0300

    add fonte externa

--- ignite/05-design-system/packages/react/dist/index.d.ts
+++ ignite/05-design-system/packages/react/dist/index.d.ts
@@ -1,11 +1,10 @@
 import * as _stitches_react_types_styled_component from '@stitches/react/types/styled-component';
 import * as _stitches_react_types_css_util from '@stitches/react/types/css-util';
+import { ComponentProps } from '@stitches/react';
 
-type ButtonProps = {
-    size: 'small' | 'big';
-};
+type ButtonProps = ComponentProps<typeof Button>;
 declare const Button: _stitches_react_types_styled_component.StyledComponent<"button", {
-    size?: "small" | "big" | undefined;
+    size?: "big" | "small" | undefined;
 }, {}, _stitches_react_types_css_util.CSS<{}, {
     colors: {
         white: string;
