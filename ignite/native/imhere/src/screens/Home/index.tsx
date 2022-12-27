112b4c16f8e1a024462f9f80e2f10f27a266366a
Vitor Nunes <stecks10@gmail.com>
2022-12-27 16:47:20 -0300

    criando o Form

--- ignite/native/imhere/src/screens/Home/index.tsx
+++ ignite/native/imhere/src/screens/Home/index.tsx
@@ -2,13 +2,13 @@ import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from './styles';
 
-export function Home(){
-  function handleParticipantAdd(){
+export function Home() {
+  function handleParticipantAdd() {
     console.log("Voce clicou no botao de adicionar!");
   }
 
-  return(
-    <View style={styles.container}> 
+  return (
+    <View style={styles.container}>
       <Text style={styles.eventName}>
         Nome do Evento
       </Text>
@@ -16,12 +16,15 @@ export function Home(){
       <Text style={styles.eventDate}>
         Sexta, 27 de dezembro de 2022.
       </Text>
-      
-      <TextInput style={styles.input} placeholder="Nome do participante" placeholderTextColor="#6b6b6b"  />
 
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}> + </Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput style={styles.input} placeholder="Nome do participante" placeholderTextColor="#6b6b6b" />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
--- ignite/native/imhere/src/screens/Home/styles.ts
+++ ignite/native/imhere/src/screens/Home/styles.ts
@@ -1,37 +1,47 @@
-import {StyleSheet }from 'react-native'
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
+    backgroundColor: "#131016",
+    padding: 24,
   },
   eventName: {
-    color: '#fff', fontSize: 24, fontWeight: 'bold',
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
     marginTop: 48,
   },
   eventDate: {
-    color: '#cecece', fontSize: 16
-  },
-  input: {
-    backgroundColor: '#1f1e25',
-    height: 56,
-    borderRadius: 5,
-    color: '#fff',
-    padding: 16,
+    color: "#cecece",
     fontSize: 16,
   },
+  input: {
+    flex: 1,
+    backgroundColor: "#1f1e25",
+    height: 56,
+    borderRadius: 5,
+    color: "#fff",
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+  },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
   },
   button: {
     width: 56,
     height: 56,
     borderRadius: 5,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 10,
-  }
-})
+    backgroundColor: "#7159c1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
+});
