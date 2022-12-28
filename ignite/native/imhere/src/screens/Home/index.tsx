0839193b40609c1927cfb8e85c72be28e0714d43
Vitor Nunes <stecks10@gmail.com>
2022-12-28 11:43:42 -0300

    Lista de participantes ( flatlist)

--- ignite/native/imhere/src/screens/Home/index.tsx
+++ ignite/native/imhere/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList } from "react-native";
 import { Participant } from "../components/Participant";
 
 import { styles } from './styles';
@@ -32,16 +32,22 @@ export function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          participants.map(participant => (
-            <Participant
-              key={participant}
-              name={participant}
-              onRemove={() => handleParticipantRemove("Vitor")} />
-          ))
-        }
-      </ScrollView>
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove("Vitor")} />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguem chegou no evento ainda?  Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
 
     </View>
   )
